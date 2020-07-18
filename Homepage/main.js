813f60e61a0c4d44efa81a56b40e9171850d3518
vivian76 <thiagocabral477@gmail.com>
2020-07-18 18:23:55 -0300

    Botão flutuante Fale Conosco

--- Homepage/main.js
+++ Homepage/main.js
@@ -8,3 +8,28 @@ bars.onclick = function() {
     nav.classList.add('n-nav');
   }
 }
+
+let imgConosco = document.querySelector('.img-fale-conosco');
+let faleConosco = document.querySelector('.fale-conosco');
+let formFale = document.querySelector('.fale-conosco form');
+
+faleConosco.style.visibility = 'hidden';
+
+console.log(faleConosco.style.visibility == 'hidden');
+console.log(faleConosco);
+
+imgConosco.onclick = function() {
+  if(faleConosco.style.visibility === 'hidden') {
+    faleConosco.style.visibility = 'visible';
+    faleConosco.style.width = '100%';
+    faleConosco.style.height = '470px';
+    faleConosco.style.opacity = '1';
+    formFale.style.opacity = '1';
+  } else {
+    faleConosco.style.visibility = 'hidden';
+    faleConosco.style.width = '0';
+    faleConosco.style.height = '0';
+    faleConosco.style.opacity = '0';
+    formFale.style.opacity = '0';
+  }
+}
